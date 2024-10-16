c21e3271cf61c322aea6d3114b77901153a06972
Mohammed <hasnain.sajadxx@gmail.com>
2024-10-16 11:25:28 +0100

    Multiplication

--- Math.java
+++ Math.java
@@ -1,3 +1,11 @@
 public class Math {
   
+    public static int Multi(int a, int b) {
+        return a * b;
+    }
+        
+    public static void main(String[] args) {
+    int result = Multi(3, 4);
+    System.out.println("Multiplication result: " + result);
+    }
 }
