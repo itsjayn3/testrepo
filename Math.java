dd6013c79e69f672e968aacd7771a320ad0e9597
Mohammed <hasnain.sajadxx@gmail.com>
2024-10-22 13:50:54 +0100

    Multiply

--- Math.java
+++ Math.java
@@ -1,11 +1,6 @@
 public class Math {
   
-    public static int Multi(int a, int b) {
+    public int Multiply(int a, int b) {
         return a * b;
-    }
-        
-    public static void main(String[] args) {
-    int result = Multi(3, 4);
-    System.out.println("Multiplication result: " + result);
     }
 }
